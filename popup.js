2dd0533e050d5c402fd0710e9cf18b29fa88bff6
Xao <divxyng.edu@gmail.com>
2022-11-28 16:30:14 -0600

    copy paste works

--- popup.js
+++ popup.js
@@ -1,9 +1,5 @@
-const tabs = await chrome.tabs.query({
-    url: [
-      "https://developer.chrome.com/docs/webstore/*",
-      "https://developer.chrome.com/docs/extensions/*",
-    ],
-});
+let dataString='';
+const tabs = await chrome.tabs.query({ currentWindow: true });
 
 const collator = new Intl.Collator();
 tabs.sort((a, b) => collator.compare(a.title, b.title));
@@ -12,14 +8,14 @@ const template = document.getElementById("li_template");
 const elements = new Set();
 for (const tab of tabs) {
   const element = template.content.firstElementChild.cloneNode(true);
+  console.log(`${JSON.stringify(tab, null, 2)}`);
 
-  const title = tab.title.split("-")[0].trim();
-  const pathname = new URL(tab.url).pathname.slice("/docs".length);
-
+  const title = tab.title; 
+  const tabUrl = tab.url;
+  dataString+=`${title}+\n+${tabUrl}+\n\n`;
   element.querySelector(".title").textContent = title;
-  element.querySelector(".pathname").textContent = pathname;
+  element.querySelector(".pathname").textContent = tabUrl;
   element.querySelector("a").addEventListener("click", async () => {
-    // need to focus window as well as the active tab
     await chrome.tabs.update(tab.id, { active: true });
     await chrome.windows.update(tab.windowId, { focused: true });
   });
@@ -31,7 +27,9 @@ document.querySelector("ul").append(...elements);
 
 const button = document.querySelector("button");
 button.addEventListener("click", async () => {
-  const tabIds = tabs.map(({ id }) => id);
-  const group = await chrome.tabs.group({ tabIds });
-  await chrome.tabGroups.update(group, { title: "DOCS" });
+  const type = "text/plain";
+  const blob = new Blob([dataString], { type });
+  const data = [new ClipboardItem({ [type]: blob })];
+
+  await navigator.clipboard.write(data);
 });
